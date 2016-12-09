e0cf9047075331884aa6a082349e35f9fdaf086d
Jessica Prado <jessica.marie.prado@gmail.com>
2016-12-09 12:55:22 -0500

    html and js file

---
+++ tableFilter.js
@@ -0,0 +1,42 @@
+var ali = {
+	data : wishes, 
+	data : dessert party, 
+	data : bridal,  
+	data : proposal, 
+	data : escape, 
+	data : upgraded escape, 
+	data : memories, 
+	data : braodway magic,  
+	data : candid documentation, 
+	data : group photo, 
+	data : marketing, 
+	data : on-location studio, 
+	data : photopass, 	
+}
+
+var amanda = {
+	data : wishes, 
+	data : dessert party, 
+	data : bridal, 
+	data : mk family session, 
+	data : enhanced,  
+	data : escape, 
+	data : upgraded escape, 
+	data : memories,  
+	data : candid documentation, 
+	data : headshot, 
+} 
+
+var amy = {
+	data : wishes, 
+	data : dessert party, 
+	data : bridal, 
+	data : mk family session, 
+	data : enhanced, 
+	data : escape, 
+	data : upgraded escape, 
+	data : memories,  
+	data : dpa group photo, 
+	data : group photo, 
+	data : on-location studio, 
+}
