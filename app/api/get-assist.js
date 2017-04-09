ea901578369018ce8bd5513663cccf37ebfd43c4
Jessica Prado <jessica.marie.prado@gmail.com>
2017-04-09 01:15:58 -0400

    fixed server

--- app/api/get-assist.js
+++ app/api/get-assist.js
@@ -1,4 +1,4 @@
-var table = require('../app/schedulingTools/libraries/assists.js');
+var table = require('../schedulingTools/libraries/assists.js');
 
 module.exports = function(app) {
     app.get('/api/assists', function(req, res) {
