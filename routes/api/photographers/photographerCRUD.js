eda180637d392bde3f1c66b8884384d66e68a3d2
Jessica Prado <jessica.marie.prado@gmail.com>
2017-05-10 17:25:25 -0400

    route

--- routes/api/photographers/photographerCRUD.js
+++ routes/api/photographers/photographerCRUD.js
@@ -3,5 +3,6 @@ var table = require('../../../app/schedulingTools/libraries/photographers.js');
 module.exports = function(app) {
     app.get('/api/photographers', function(req, res) {
         res.send(table);
+        console.log("Photo GET complete");
     });
 }
