a64e89a1f6b7ada5411d2d215ecf330079a99665
Jessica Prado <jessica.marie.prado@gmail.com>
2017-05-13 17:05:07 -0400

    gulp compress added

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 gulp.task('syntax', function() {
     return gulp
@@ -10,3 +12,13 @@ gulp.task('syntax', function() {
         .pipe(jscs())
         .pipe(jscs.reporter());
 });
+
+gulp.task('compress-js', function(){
+	return gulp
+		.src([
+	    	'./app/**/*.js',
+	    	'./*.js'
+    	])
+    	.pipe(uglify())
+    	.pipe(gulp.dest('./app/schedulingTools/dist'))
+})
