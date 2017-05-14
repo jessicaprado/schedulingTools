0f9ff5570b9703184d826fb869b629a39f49d573
Jessica Prado <jessica.marie.prado@gmail.com>
2017-05-14 17:36:01 -0400

    switching computers

--- gulpFile.js
+++ gulpFile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var jscs = require('gulp-jscs');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var css = require('gulp-css');
 
 gulp.task('syntax', function() {
     return gulp
@@ -13,6 +15,13 @@ gulp.task('syntax', function() {
         .pipe(jscs.reporter());
 });
 
+gulp.task('css', function() {
+	gulp
+	.src('.app/schedulingTools/layout/assets/style/css')
+	.pipe(cssMin())
+	.pipe(gulp.dest('.app/schedulingTools/layout/assets/style/css'))
+})
+
 gulp.task('compress-js', function(){
 	return gulp
 		.src([
@@ -21,4 +30,11 @@ gulp.task('compress-js', function(){
     	])
     	.pipe(uglify())
     	.pipe(gulp.dest('./app/schedulingTools/dist'))
+})
+
+gulp.task('sass', function(){
+	return gulp
+		.src('.app/schedulingTools/layout/assets/style/scss/style.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('.app/schedulingTools/layout/assets/style/css'))
 })
