5a636ebb2cc8e98ef47e23aaf8cc959506aab291
Jessica Prado <jessica.marie.prado@gmail.com>
2017-05-15 10:23:34 -0400

    .DS_Store banished

--- gulpFile.js
+++ gulpFile.js
@@ -4,6 +4,7 @@ var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var css = require('gulp-css');
+var cleanCSS = require('gulp-clean-css')
 
 gulp.task('syntax', function() {
     return gulp
@@ -15,11 +16,11 @@ gulp.task('syntax', function() {
         .pipe(jscs.reporter());
 });
 
-gulp.task('css', function() {
-	gulp
-	.src('.app/schedulingTools/layout/assets/style/css')
-	.pipe(cssMin())
-	.pipe(gulp.dest('.app/schedulingTools/layout/assets/style/css'))
+gulp.task('minify-css', function() {
+	return gulp
+		.src('app/schedulingTools/layout/assets/style/css/style.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('app/schedulingTools/client'))
 })
 
 gulp.task('compress-js', function(){
